15de28bb73350f27ad3e836145913b0c13f512c2
elenaJO <ele.lucero1993@gmail.com>
2018-03-02 20:53:08 -0500

    agregando componente title

---
+++ src/title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './title.css';
+
+class Title extends Component {
+  render() {
+    return (
+      <div className="container-title">
+      <h1>Grace Hopper</h1>
+      </div>
+    );
+  }
+}
+
+export default Title;
